5a4a6820533c55bf1249254c937bdc6c91d15949
James Wallace <jrwallace@fastmail.co.uk>
2026-05-22 06:43:50 +0100

    Adding Initial PowerShell file to detect where scavenging is enabled in the domain.

---
+++ WhereIsScavenging.ps1
@@ -0,0 +1,10 @@
+$DCs = Import-Csv ".\AllDCs.csv"
+$DCs | Add-Member -MemberType NoteProperty -Name Enabled -Value '--'
+Foreach ($DC in $Dcs) {
+    "Checking $($DC.Name)"
+    $Check = Get-DnsServerScavenging -ComputerName $($DC.Name)
+    $DC.Enabled = $Check.ScavengingState
+    $Check = $null
+}
+
+$DCs | Export-Csv -NoTypeInformation ".\AllDCsChecked.csv"
