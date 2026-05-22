7ae2889fa7f37bce7ea68ea198663ed9e289f6b4
James Wallace <jrwallace@fastmail.co.uk>
2026-05-22 07:11:02 +0100

    Updated Scavenging script

--- WhereIsScavenging.ps1
+++ WhereIsScavenging.ps1
@@ -1,3 +1,10 @@
+<# 
+    Used to dynamically determine where Scavenging is enabled on each Domain Controller in each domain.
+    Will require appropriate rights on each DC on each domain to run
+
+    V1 - jrw452 - Inital Version
+#>
+
 $DCs = Import-Csv ".\AllDCs.csv"
 $DCs | Add-Member -MemberType NoteProperty -Name Enabled -Value '--'
 Foreach ($DC in $Dcs) {
