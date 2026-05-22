b7fadeba8ecab69fb68d5f9f7dc5ff730031e5d0
James Wallace <jrwallace@fastmail.co.uk>
2026-05-22 12:18:33 +0100

    Updated DNS Scavenging checker to V2 for robustness

--- WhereIsScavenging.ps1
+++ WhereIsScavenging.ps1
@@ -1,16 +1,28 @@
 <# 
-    Used to dynamically determine where Scavenging is enabled on each Domain Controller in each domain.
-    Will require appropriate rights on each DC on each domain to run
+    Used to dynamically determine where Scavenging is enabled on each Domain Controller in each domain. Whilst you can theoretically enable it on a RODC this is bad practice, but is included here for completeness. As well as the Domain Controller, don't forget to check which zones have scavenging enabled.
+    Will require appropriate rights on each DC to run.
 
     V1 - jrw452 - Inital Version
+    V2 - jrw452 - Updated for dynamic discovery of DCs.
 #>
 
-$DCs = Import-Csv ".\AllDCs.csv"
-$DCs | Add-Member -MemberType NoteProperty -Name Enabled -Value '--'
+Import-Module ActiveDirectory
+
+$DCs = Get-ADDomainController -filter * | select Hostname,IsReadOnly,ScavengingEnabled
+
 Foreach ($DC in $Dcs) {
-    "Checking $($DC.Name)"
-    $Check = Get-DnsServerScavenging -ComputerName $($DC.Name)
-    $DC.Enabled = $Check.ScavengingState
+    "Checking $($DC.HostName)"
+    try {
+        $Check = Get-DnsServerScavenging -ComputerName $($DC.HostName)
+        if ($Check) {
+            $DC.ScavengingEnabled = $Check.ScavengingState
+        } else {
+            $DC.ScavengingEnabled = "No Data returned"
+        }
+    } catch {
+        $DC.ScavengingEnabled = "Failed. Error is $($_.exception.message)"
+    }
     $Check = $null
 }
+
 $DCs | Export-Csv -NoTypeInformation ".\AllDCsChecked.csv"
